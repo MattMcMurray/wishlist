65b12c80433dffc7b0846bc3398721637407a974
Matt McMurray <mathieumcmurray@gmail.com>
2024-10-03 19:37:09 -0500

    Sentry configuration

--- Gemfile
+++ Gemfile
@@ -40,6 +40,10 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem "stackprof"
+gem "sentry-ruby"
+gem "sentry-rails"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -66,3 +70,5 @@ end
 gem "tailwindcss-rails", "~> 2.7"
 
 gem "pundit", "~> 2.4"
+
+gem "dockerfile-rails", ">= 1.6", group: :development
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = "https://80514efbc67ba25c9de9de51a12fdd17@o4508061652877312.ingest.us.sentry.io/4508061653139456"
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for tracing.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 1.0
+end
