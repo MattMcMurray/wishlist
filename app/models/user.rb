49168e205e4ad20bf2cb07a847eaa5860b25b274
Matt McMurray <mathieumcmurray@gmail.com>
2024-10-04 08:40:41 -0500

    Styling updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :lists, dependent: :destroy
   has_many :list_shares, dependent: :destroy
-  has_many :shared_withs, dependent: :destroy, class_name: "ListShare"
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   validates :email_address, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
