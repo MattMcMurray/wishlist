163fda7455dc14b1029ff547d25f794c61d7cc59
Matt McMurray <mathieumcmurray@gmail.com>
2024-10-04 09:19:17 -0500

    Sentry: ignore healthcheck requests

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,7 +7,7 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for tracing.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  # config.traces_sample_rate = 1.0
   # or
   # config.traces_sampler = lambda do |context|
   #   true
@@ -17,4 +17,24 @@ Sentry.init do |config|
   # of sampled transactions.
   # We recommend adjusting this value in production.
   config.profiles_sample_rate = 1.0
+
+  config.traces_sampler = lambda do |sampling_context|
+    transaction_context = sampling_context[:transaction_context]
+
+    op = transaction_context[:op]
+    transaction_name = transaction_context[:name]
+
+    case op
+    when /http/
+      case transaction_name
+      when /health/
+        # ignore http trace for healthcheck requests to avoid noise in performance metrics
+        0.0
+      else
+        1.0
+      end
+    else
+      1.0
+    end
+  end
 end
