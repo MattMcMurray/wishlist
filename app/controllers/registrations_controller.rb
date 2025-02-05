eaa962d280c620898f1b355bc1520721c55edabf
Matt McMurray <mathieumcmurray@gmail.com>
2025-02-05 16:11:34 -0600

    Disallow no-reply domains for registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < ApplicationController
+  DISALLOWED_DOMAINS = [ "dont-reply.me", "do-not-respond.me" ]
+
   allow_unauthenticated_access only: %i[ new create ]
   before_action :resume_session, only: [ :new ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
@@ -14,6 +16,16 @@ class RegistrationsController < ApplicationController
 
     if permitted["password"] != permitted["confirm_password"]
       return redirect_to new_registration_url, alert: "Password confirmation does not match"
+    end
+
+    # Check if the email address contains any of the disallowed domains
+    if DISALLOWED_DOMAINS.any? { |domain| permitted["email_address"].end_with?(domain) }
+      Sentry.add_breadcrumb(Sentry::Breadcrumb.new(
+        category: "registration",
+        data: { **permitted }
+      ))
+      Sentry.capture_message("Disallowed user registration")
+      return redirect_to new_registration_url, alert: "Disallowed"
     end
 
     user = User.new(email_address: permitted["email_address"], password: permitted["password"])
